61597c36e454901ad45b852b90d3e40b6d646888
natassia-nn <natassianguyen@gmail.com>
2025-02-19 10:21:46 -0800

    finished

--- Main.java
+++ Main.java
@@ -7,16 +7,31 @@ public class Main
   {
     Scanner sc = new Scanner(System.in);
     String input = "";
-    input = sc.nextLine();
+    
 
     ArrayList<String> list = new ArrayList<String>();
     
-
+    System.out.println("Please enter words, enter STOP to stop adding");
     while (!input.equals("STOP"))
     {
-      list.add(input);
+      input = sc.nextLine();
+      
+      if (!input.equals("STOP"))
+      {
+        list.add(input);
+      }
+
     }
-      System.out.println(list.size());
-      System.out.println(list);
+     System.out.println(list.size());
+     System.out.println(list);
+
+     if (list.size() > 2)
+     {
+        String word = list.get(0);
+        list.remove(word);
+        list.set(list.size()-1, word);
+        System.out.println(list);
+
+     }
   }
 }
